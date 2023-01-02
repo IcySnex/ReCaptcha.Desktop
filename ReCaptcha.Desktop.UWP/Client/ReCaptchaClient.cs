23c017d495e0d8e379858aeef52a949a2c418374
IcySnex <lao43919@gmail.com>
2023-01-02 15:14:37 +0100

    Added ReCaptcha.Desktop.UWP

---
+++ ReCaptcha.Desktop.UWP/Client/ReCaptchaClient.cs
@@ -0,0 +1,13 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReCaptcha.Desktop.UWP.Client;
+
+public class ReCaptchaClient
+{
+}
