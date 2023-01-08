cf736e3529893ca0b435c9fa43685c5b6a8fbcc4
IcySnex <lao43919@gmail.com>
2023-01-08 16:56:02 +0100

    Added themes to UWP

---
+++ ReCaptcha.Desktop.UWP/UI/Themes/DarkTheme.cs
@@ -0,0 +1,96 @@
+﻿using ReCaptcha.Desktop.UWP.UI.Themes.Interfaces;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
+
+namespace ReCaptcha.Desktop.UWP.UI.Themes;
+
+/// <summary>
+/// Dark theme for ReCaptcha control
+/// </summary>
+public class DarkTheme : ITheme
+{
+    /// <summary>
+    /// The main backhround color
+    /// <br/>
+    /// Setter will not update after initialition. 'Init-Only' is not used because of a bug in WinUI3.
+    /// </summary>
+    public Brush Background { get; set; } = new SolidColorBrush(Color.FromArgb(255, 34, 34, 34));
+
+    /// <summary>
+    /// The main border color
+    /// <br/>
+    /// Setter will not update after initialition. 'Init-Only' is not used because of a bug in WinUI3.
+    /// </summary>
+    public Brush Border { get; set; } = new SolidColorBrush(Color.FromArgb(255, 72, 72, 72));
+
+    /// <summary>
+    /// The main foreground color
+    /// <br/>
+    /// Setter will not update after initialition. 'Init-Only' is not used because of a bug in WinUI3.
+    /// </summary>
+    public Brush Foreground { get; set; } = new SolidColorBrush(Color.FromArgb(255, 255, 255, 255));
+    /// <summary>
+    /// The secondary foreground color
+    /// <br/>
+    /// Setter will not update after initialition. 'Init-Only' is not used because of a bug in WinUI3.
+    /// </summary>
+    public Brush ForegroundSecondary { get; set; } = new SolidColorBrush(Color.FromArgb(255, 170, 170, 170));
+
+    /// <summary>
+    /// The error message color
+    /// <br/>
+    /// Setter will not update after initialition. 'Init-Only' is not used because of a bug in WinUI3.
+    /// </summary>
+    public Brush Error { get; set; } = new SolidColorBrush(Color.FromArgb(255, 255, 0, 0));
+
+    /// <summary>
+    /// The checkbox background color
+    /// <br/>
+    /// Setter will not update after initialition. 'Init-Only' is not used because of a bug in WinUI3.
+    /// </summary>
+    public Brush CheckBoxBackground { get; set; } = new SolidColorBrush(Color.FromArgb(255, 40, 40, 40));
+    /// <summary>
+    /// The checkbox background color when hovered
+    /// <br/>
+    /// Setter will not update after initialition. 'Init-Only' is not used because of a bug in WinUI3.
+    /// </summary>
+    public Brush CheckBoxBackgroundHover { get; set; } = new SolidColorBrush(Color.FromArgb(255, 40, 40, 40));
+    /// <summary>
+    /// The checkbox background color when pressed
+    /// <br/>
+    /// Setter will not update after initialition. 'Init-Only' is not used because of a bug in WinUI3.
+    /// </summary>  
+    public Brush CheckBoxBackgroundPressed { get; set; } = new SolidColorBrush(Color.FromArgb(255, 60, 60, 60));
+
+    /// <summary>
+    /// The checkbox border color
+    /// <br/>
+    /// Setter will not update after initialition. 'Init-Only' is not used because of a bug in WinUI3.
+    /// </summary>
+    public Brush CheckBoxBorder { get; set; } = new SolidColorBrush(Color.FromArgb(255, 84, 84, 84));
+    /// <summary>
+    /// The checkbox border color when hovered
+    /// <br/>
+    /// Setter will not update after initialition. 'Init-Only' is not used because of a bug in WinUI3.
+    /// </summary>
+    public Brush CheckBoxBorderHover { get; set; } = new SolidColorBrush(Color.FromArgb(255, 100, 100, 100));
+    /// <summary>
+    /// The checkbox border color when pressed
+    /// <br/>
+    /// Setter will not update after initialition. 'Init-Only' is not used because of a bug in WinUI3.
+    /// </summary>
+    public Brush CheckBoxBorderPressed { get; set; } = new SolidColorBrush(Color.FromArgb(255, 115, 115, 115));
+
+    /// <summary>
+    /// The checkbox loading spinner color
+    /// <br/>
+    /// Setter will not update after initialition. 'Init-Only' is not used because of a bug in WinUI3.
+    /// </summary>
+    public Brush CheckBoxSpinner { get; set; } = new SolidColorBrush(Color.FromArgb(255, 78, 144, 245));
+    /// <summary>
+    /// The checkbox checkmark color
+    /// <br/>
+    /// Setter will not update after initialition. 'Init-Only' is not used because of a bug in WinUI3.
+    /// </summary>
+    public Brush CheckBoxCheckmark { get; set; } = new SolidColorBrush(Color.FromArgb(255, 0, 158, 66));
+}
---
+++ ReCaptcha.Desktop.UWP/UI/Themes/Interfaces/ITheme.cs
@@ -0,0 +1,84 @@
+﻿using Windows.UI.Xaml.Media;
+
+namespace ReCaptcha.Desktop.UWP.UI.Themes.Interfaces;
+
+/// <summary>
+/// Theme for ReCaptcha control
+/// </summary>
+public interface ITheme
+{
+    /// <summary>
+    /// The main backhround color
+    /// </summary>
+    public Brush Background { get; }
+
+    /// <summary>
+    /// The main border color
+    /// </summary>
+    public Brush Border { get; }
+
+    /// <summary>
+    /// The main foreground color
+    /// </summary>
+    public Brush Foreground { get; }
+    /// <summary>
+    /// The secondary foreground color
+    /// </summary>
+    public Brush ForegroundSecondary { get; }
+
+    /// <summary>
+    /// The error message color
+    /// </summary>
+    public Brush Error { get; }
+
+
+    /// <summary>
+    /// The checkbox background color
+    /// </summary>
+    public Brush CheckBoxBackground { get; }
+    /// <summary>
+    /// The checkbox background color when hovered
+    /// </summary>
+    public Brush CheckBoxBackgroundHover { get; }
+    /// <summary>
+    /// The checkbox background color when pressed
+    /// </summary>
+    public Brush CheckBoxBackgroundPressed { get; }
+
+    /// <summary>
+    /// The checkbox border color
+    /// </summary>
+    public Brush CheckBoxBorder { get; }
+    /// <summary>
+    /// The checkbox border color when hovered
+    /// </summary>
+    public Brush CheckBoxBorderHover { get; }
+    /// <summary>
+    /// The checkbox border color when pressed
+    /// </summary>
+    public Brush CheckBoxBorderPressed { get; }
+
+    /// <summary>
+    /// The checkbox loading spinner color
+    /// </summary>
+    public Brush CheckBoxSpinner { get; }
+    /// <summary>
+    /// The checkbox checkmark color
+    /// </summary>
+    public Brush CheckBoxCheckmark { get; }
+
+
+    ///// <summary>
+    ///// Creates a new light theme
+    ///// </summary>
+    ///// <returns>A new light theme</returns>
+    //public static ITheme Light() =>
+    //    new LightTheme();
+
+    ///// <summary>
+    ///// Creates a new dark theme
+    ///// </summary>
+    ///// <returns>A new dark theme</returns>
+    //public static ITheme Dark() =>
+    //    new DarkTheme();
+}
