3044349f7884672d8796af5549348decc9468f1b
IcySnex <81762750+IcySnex@users.noreply.github.com>
2023-03-12 11:56:10 +0100

    Started working on documenation (VuePress)

---
+++ Documentation/docs/.vuepress/config.js
@@ -0,0 +1,40 @@
+import { defineUserConfig } from 'vuepress'
+import { defaultTheme } from '@vuepress/theme-default'
+import { searchPlugin } from '@vuepress/plugin-search'
+
+export default defineUserConfig({
+  lang: 'en-US',
+  title: 'ReCaptcha.Desktop',
+  description: 'Access Google reCAPTCHA on all major windows desktop frameworks (WPF, WinUI, UWP, Winforms, Console) ',
+
+  head: [[ 'link', { rel: 'icon', href: '/icon.svg' }]],
+  theme: defaultTheme({
+    colorMode: 'dark',
+    logo: '/icon.svg',
+
+    repo: 'IcySnex/ReCaptcha.Desktop',
+    editLinkText: 'Edit this page on GitHub',
+
+    docsRepo: 'IcySnex/ReCaptcha.Desktop',
+    docsBranch: 'docs',
+    docsDir: 'docs',
+
+    sidebar: {
+      '/guide': [
+        {
+          text: 'Guide',
+          children: [ '/guide', '/guide/getting-started' ],
+        }
+      ]
+    },
+
+    navbar: [{ text: 'Guide', link: '/guide/getting-started' }]
+  }),
+
+  plugins: [
+    searchPlugin({
+      maxSuggestions: 10,
+      locales: { '/': { placeholder: 'Search' }}
+    })
+  ]
+})
---
+++ Documentation/docs/index.md
@@ -0,0 +1,29 @@
+---
+home: true
+
+title: Home
+heroImage: /icon.svg
+heroHeight: 120
+
+actions:
+  - text: Get Started ➜
+    link: /guide/getting-started
+    type: primary
+  - text: About
+    link: /about
+    type: secondary
+
+features:
+  - title: Across all frameworks
+    details: Use Google reCAPTCHA for all your projects with just a few lines of code, regardless of the desktop frameworks.
+  - title: Customizable
+    details: From themes to fundamental components! ReCaptcha.Desktop allows you to customize just about anything.
+  - title: Fast & Reliable
+    details: ReCaptcha.Desktop uses WebView2 and the official reCAPTCHA widget to bring you the best performance and stability.
+
+footer: '
+This is an open source library and not affiliated with Google in any way.
+<a href="/license.html">License</a>
+'
+footerHtml: true
+---
